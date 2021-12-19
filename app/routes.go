da9a09d0b9c23cd7f079dacf7877524858dcce42
dhikaroofi <roofi.dhika@gmail.com>
2021-12-19 22:15:02 +0700

    update

--- app/routes.go
+++ app/routes.go
@@ -4,6 +4,8 @@ import "github.com/dhikaroofi/go/app/controller"
 
 func (a *Mojoo) setRouters() {
 	a.Post("/login", a.guest(controller.AuthLogin))
+	a.Get("/merchant/{merchant_id}/list", a.guest(controller.GetDailyReportMerchant))
+	a.Get("/{merchant_id}/outlet/list", a.guest(controller.GetDailyReportMerchant))
 	a.Get("/report/merchant/{merchant_id}/omzet", a.guest(controller.GetDailyReportMerchant))
 	a.Get("/report/outlet/{outlet_id}/omzet", a.guest(controller.GetDailyReportOutlet))
 }
